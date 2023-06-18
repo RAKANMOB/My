626bad69a7179e26cd3582f6fa59634598d58f2e
RAKAN <ALMUBAYEDH84@GMAIL.COM>
2023-06-18 18:07:20 +0300

    my first comunt

--- inc/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost', 'root','root','project');
-
-if(!$conn){
-    echo'Error: '  . mysqli_connect_error();
-}
--- parts/footer.php
+++
@@ -1,7 +0,0 @@
-<script src="./js/bootstrap.bundle.min.js"></script>
-<script src="./js/loader.js"></script>
-    <script src="./js/script.js"></script>
-
-
-</body>
-</html>
